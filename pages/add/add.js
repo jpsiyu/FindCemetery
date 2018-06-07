42fd402f93050f736429af660d0aa25d75cd6b2d
jpsiyu <jpsiyu@163.com>
2018-06-07 16:38:01 +0800

    improve page add ui

--- pages/add/add.js
+++ pages/add/add.js
@@ -12,7 +12,12 @@ Page({
     lngLat: '',
     addrName: '未设置',
     name: null,
+    radioItems: [],
+    showTopTips: false,
+    errMsg: '',
   },
+
+  timeoutHandler: null,
 
   chooseLocation: function () {
 
@@ -33,34 +38,80 @@ Page({
     wx.chooseLocation({success: successCallback})
   },
 
+  fromCheck(formData){
+    let msg = ''
+    let ok = false
+    const nameLen = formData.name.length
+    const reg=/^[0-9]+.?[0-9]*$/
+    const isNumber = reg.test(formData.age)
+
+    if(nameLen <=0 || nameLen > 6){
+      msg = '名字长度0到6个字符'
+    }else if(!isNumber){
+      msg = '年龄字段输入有误'
+    }else{
+      ok = true
+    }
+    return {ok, msg}
+  },
+
+  showTopTips: function(){
+    this.setData({showTopTips: true})
+    if(this.timeoutHandler) clearTimeout(this.timeoutHandler)
+    this.timeoutHandler = setTimeout(() => {this.setData({showTopTips: false})}, 3000)
+  },
+
   /**
    * 表单提交
    */
   formSubmit: function(event){
-    var inputName = event.detail.value.inputName
+    const checkRes = this.fromCheck(event.detail.value)
+    if(!checkRes.ok){
+      this.setData({errMsg: checkRes.msg})
+      this.showTopTips()
+      return
+    }
+    let inputName = event.detail.value.name
     if (inputName == '')
       inputName = "无名"
+    const age = Number(event.detail.value.age)
     
     const successCallback = (stone) => {
-      console.log('add stone', stone)
       app.dataholder.addCemetery(stone)
       wx.switchTab({url: '/pages/main/main'})
     }
 
     app.requestAddStone(
       inputName, 
-      90,
+      age,
       [this.data.longitude, this.data.latitude],
       this.data.addrName,
       successCallback,
     )
   },
 
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      radioItems: [
+        {value: 'male', name: '男性', checked: true},
+        {value: 'female', name: '女性', checked: false},
+      ]
+    })
+  },
+
+  radioChange: function(event){
+    const choose = event.detail.value
+    this.data.radioItems.map( item => {
+      if(item.value === choose)
+        item.checked = true
+      else
+        item.checked = false
+    })
+    this.setData({radioItems: this.data.radioItems})
   },
 
   /**
